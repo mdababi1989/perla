45c2d58b6546587069f007601ccb78d5f2a7efd7
mdababi <mdababi@192.168.0.101>
2019-02-11 00:52:42 +0100

    create home page

---
+++ src/main/java/com/perlaagriculture/PerlaagricultureApplication.java
@@ -0,0 +1,14 @@
+package com.perlaagriculture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerlaagricultureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerlaagricultureApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/perlaagriculture/controller/PerlaAgricultureController.java
@@ -0,0 +1,35 @@
+package com.perlaagriculture.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PerlaAgricultureController {
+	
+	
+	@GetMapping("/")
+	public String rootPage() {
+		return "redirect:accueil";
+	}
+	
+	@GetMapping("/accueil")
+	public String homePage() {
+		return "index";
+	}
+	
+	@GetMapping("/2")
+	public String homePag() {
+		return "index2";
+	}
+	
+	@GetMapping("/aFaire")
+	public String aFairePag() {
+		return "afaire";
+	}
+	
+	@GetMapping("/services")
+	public String noServices() {
+		return "services";
+	}
+
+}
